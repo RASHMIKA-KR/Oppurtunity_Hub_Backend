b6333335e6024528aa42df9d23e6b7df5b236631
RASHMIKA-KR <krrashmika2004@gmail.com>
2024-06-09 19:41:24 +0530

    dbName changed

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const dbConnection = () => {
   mongoose
     .connect(process.env.MONGO_URI, {
-      dbName: "jobSeek",
+      dbName: "Oppurtunity_Hub",
     })
     .then(() => {
       console.log("Connected to database.");
